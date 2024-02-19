ad80845617d62100aceb82139922897397e369ac
Jan Carbonell <jcllobet@gmail.com>
2024-02-18 21:32:30 -0500

    template setup

---
+++ src/Message.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Message = () => {
+  const name = "Jan";
+  return (
+    if (name){
+      <div>
+        <h1>Hello {name}</h1>
+      </div>
+    }
+    else{
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    }
+  );
+};
+
+export default Message;
